1835abab0398af605e582865b362c3ffd311da1a
Eric S. Londres <elondres@stevens.edu>
2019-07-24 21:29:23 -0400

    started Firebase integration

---
+++ src/favorites.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyDeGcCLR3rxUHnSTnX11pPkPZzKQpn7wqA",
+    authDomain: "slondr-site.firebaseapp.com",
+    databaseURL: "https://slondr-site.firebaseio.com",
+    projectId: "slondr-site",
+    storageBucket: "",
+    messagingSenderId: "1033960889693",
+    appId: "1:1033960889693:web:a4288d0ff950999f"
+});
+
