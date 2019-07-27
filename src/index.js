3db80e9e1e52e491277f39011c3ebd77511bbb03
Eric S. Londres <elondres@stevens.edu>
2019-07-27 17:21:12 -0400

    More code refactoring and cleanup

--- src/index.js
+++ src/index.js
@@ -1,45 +1,31 @@
 const uri = 'https://api.slondr.ml/favorites';
 
-const getLinkCount = id => {
+const getLinkCount = node => {
     var linkCount;
-    fetch(uri + id).then(result => result.json()).then(response => document.getElementById(id).innerHTML += ` (${response.count})`).catch(error => console.error(error));
+    fetch(uri + node.id)
+	.then(result => result.json())
+	.then(response => node.innerHTML += ` (${response.count})`)
+	.catch(error => console.error(error));
 };
 
-const $ = id => {
-    getLinkCount(id);
-    document.getElementById(id).addEventListener('click', event => {
-	event.preventDefault();
-	(async id => {
-	    const response = await fetch(uri, {
-		method: 'POST',
-		body: JSON.stringify({ id: id }),
-		headers: { 'Content-Type': 'application/json' }
+const $ = () => {
+    document.querySelectorAll('.favorite').forEach(node => {
+	getLinkCount(node);
+	node.addEventListener('click', event => {
+	    event.preventDefault();
+	    (async id => {
+		const response = await fetch(uri, {
+		    method: 'POST',
+		    body: JSON.stringify({ id: id }),
+		    headers: { 'Content-Type': 'application/json' }
+		});
+		return response;
+	    })(node.id).then(response => {
+		console.log(response.status);
+		window.location = node.href;
 	    });
-	    return response;
-	})(id).then(response => {
-	    console.log(response.status);
-	    window.location = document.getElementById(id).href;
 	});
     });
 };
 
-$('gitlab');
-$('mystevens');
-$('todoist');
-$('peoplefinder');
-$('canvas');
-$('apota');
-$('workday');
-$('youtube');
-$('ducklink');
-$('virtualems');
-$('googlemessages');
-$('play');
-$('groupme');
-$('catering');
-$('github');
-$('teamdynamix');
-$('kek');
-$('intervalcalculator');
-$('songs');
-$('chords');
+$;
