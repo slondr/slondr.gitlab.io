3328aef6a05e08b7961870870e98f9b749f0c629
Eric S. Londres <elondres@stevens.edu>
2019-07-27 17:43:26 -0400

    removed dead code

--- src/index.js
+++ src/index.js
@@ -20,19 +20,6 @@ document.querySelectorAll('.favorite').forEach(node => {
     getLinkCount(node);
     node.addEventListener('click', event => {
 	event.preventDefault();
-	/* (async id => {
-	    const response = await fetch(uri, {
-		method: 'POST',
-		body: JSON.stringify({ id: id }),
-		headers: { 'Content-Type': 'application/json' }
-	    });
-	    return response;
-	})(node.id).then(response => {
-	    console.log(response.status);
-	    window.location = node.href;
-	});
-	*/
-
 	updateLinkCount(node.id)
 	    .then(response => {
 		console.log(response.status);
